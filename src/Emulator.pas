7233a3203673b9dcaac37035bf84ebb8bae6ed7d
Tibor Nagy <xnagytibor@gmail.com>
2020-03-28 03:03:46 +0100

    Better mnemonics

--- src/Emulator.pas
+++ src/Emulator.pas
@@ -43,148 +43,148 @@ type
     ocReserved6            =  -6, { <??> - Undefined     }
     ocReserved5            =  -5, { <??> - Undefined     }
     ocRotate               =  -4, { RGTR - ROTR RD RA RB }
-    ocShift                =  -3, { RGTR - SHFT RD RA RB }
+    ocShift                =  -3, { RGTR - LSHR RD RA RB }
     ocNegation             =  -2, { RGTR - NEGR RD RA    }
     ocDyadicFunction       =  -1, { RGTR - DYAD RD RA RB }
     ocAddRegister          =   0, { RGTR - ADDR RD RA RB }
-    ocAddImmediateShort    =   1, { IMM3 - ADSI RD RA 12 }
-    ocAddImmediateHalf     =   2, { IMM6 - ADHI RD 123   }
-    ocLoadLowImmediate     =   3, { IMM6 - LDLI RD 123   }
-    ocLoadHighImmediate    =   4, { IMM6 - LDHI RD 123   }
+    ocAddImmediateQuarter  =   1, { IMM3 - ADDQ RD RA 12 }
+    ocAddImmediateHalf     =   2, { IMM6 - ADDH RD 123   }
+    ocLoadLowImmediate     =   3, { IMM6 - LDLH RD 123   }
+    ocLoadHighImmediate    =   4, { IMM6 - LDHH RD 123   }
     ocLoadMemory           =   5, { RGTR - LDMR RD RA RB }
     ocStoreMemory          =   6, { RGTR - STMR RD RA RB }
     ocBranchEquals         =   7, { RGTR - BREQ RD RA RB }
     ocBranchNotEquals      =   8, { RGTR - BRNE RD RA RB }
     ocBranchLessThan       =   9, { RGTR - BRLT RD RA RB }
     ocBranchLessEqualsThan =  10, { RGTR - BRLE RD RA RB }
-    ocPush                 =  11, { RGTR - PSHR SP RA    }
-    ocPop                  =  12, { RGTR - POPR SP RA    }
+    ocPush                 =  11, { RGTR - PUSH SP RA    }
+    ocPull                 =  12, { RGTR - PULL SP RA    }
     ocCall                 =  13  { RGTR - CALL SP RA    }
     {
       General pseudo opcodes - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-        NoOperation              NOOP            => ADDR R0 R0 R0
+        NoOperation              NOOP            => ADDR S0 S0 S0
 
-        Move                     MOVR RD RA      => ADDR RD RA R0
+        Move                     MOVR RD RA      => ADDR RD RA S0
 
-        Return                   RTRN SP         => POPR SP PC
+        Return                   RETURN SP       => PULL SP PC
 
         BranchGreaterThan        BRGT RD RA RB   => BRLE RD RB RA
 
         BranchGreaterEqualsThan  BRGE RD RA RB   => BRLT RD RB RA
 
-        LoadImmediate            LDI RD -264992  => LDHI RD -364
-                                                    ADHI RD 364
+        LoadImmediate            LDW RD -264992  => LDHH RD -364
+                                                    ADDH RD 364
 
-        LoadMemory               LDM RD 212686   => LDHI RD 292
-                                                    ADHI RD -182
-                                                    LDMR RD RD R0
+        LoadMemory               LDM RD 212686   => LDHH RD 292
+                                                    ADDH RD -182
+                                                    LDMR RD RD S0
 
         Subtraction              SUBR RD RA RB   => NEGR RD RB
                                                     ADDR RD RD RA
 
-        StackRelativeLoad        STCK SP RD -1   => ADSI RD SP -1
-                                                    LDMR RD RD R0
+        StackRelativeLoad        STACK SP RD -1  => ADDQ RD SP -1
+                                                    LDMR RD RD S0
 
       Two-operand functions using DYAD - - - - - - - - - - - - - - - - - - - - -
 
-        LogicalAnd               AND RD RA RB    => LDHI RD 8
-                                                    ADHI RD -40
+        LogicalAnd               AND RD RA RB    => LDHH RD 8
+                                                    ADDH RD -40
                                                     DYAD RD RA RB
 
-        LogicalNand              NAND RD RA RB   => LDHI RD -8
-                                                    ADHI RD 40
+        LogicalNand              NAND RD RA RB   => LDHH RD -8
+                                                    ADDH RD 40
                                                     DYAD RD RA RB
 
-        LogicalOr                OR RD RA RB     => LDHI RD 13
-                                                    ADHI RD 251
+        LogicalOr                OR RD RA RB     => LDHH RD 13
+                                                    ADDH RD 251
                                                     DYAD RD RA RB
 
-        LogicalNor               NOR RD RA RB    => LDHI RD -13
-                                                    ADHI RD -251
+        LogicalNor               NOR RD RA RB    => LDHH RD -13
+                                                    ADDH RD -251
                                                     DYAD RD RA RB
 
-        LogicalXor               XOR RD RA RB    => LDHI RD -8
-                                                    ADHI RD 8
+        LogicalXor               XOR RD RA RB    => LDHH RD -8
+                                                    ADDH RD 8
                                                     DYAD RD RA RB
 
-        LogicalXnor              XNOR RD RA RB   => LDHI RD 8
-                                                    ADHI RD -8
+        LogicalXnor              XNOR RD RA RB   => LDHH RD 8
+                                                    ADDH RD -8
                                                     DYAD RD RA RB
 
-        Equality                 TEQ RD RA RB    => LDHI RD 5
-                                                    ADHI RD -200
+        Equality                 TEQ RD RA RB    => LDHH RD 5
+                                                    ADDH RD -200
                                                     DYAD RD RA RB
 
-        LessThan                 TLT RD RA RB    => LDHI RD -13
-                                                    ADHI RD 146
+        LessThan                 TLT RD RA RB    => LDHH RD -13
+                                                    ADDH RD 146
                                                     DYAD RD RA RB
 
-        LessEqualsThan           TLE RD RA RB    => LDHI RD 5
-                                                    ADHI RD 310
+        LessEqualsThan           TLE RD RA RB    => LDHH RD 5
+                                                    ADDH RD 310
                                                     DYAD RD RA RB
 
-        GreaterThan              TGT RD RA RB    => LDHI RD -5
-                                                    ADHI RD -310
+        GreaterThan              TGT RD RA RB    => LDHH RD -5
+                                                    ADDH RD -310
                                                     DYAD RD RA RB
 
-        GreaterEqualsThan        TGE RD RA RB    => LDHI RD 13
-                                                    ADHI RD -146
+        GreaterEqualsThan        TGE RD RA RB    => LDHH RD 13
+                                                    ADDH RD -146
                                                     DYAD RD RA RB
 
-        Consensus                CSS RD RA RB    => LDHI RD 9
-                                                    ADHI RD -1
+        Consensus                CSS RD RA RB    => LDHH RD 9
+                                                    ADDH RD -1
                                                     DYAD RD RA RB
 
-        AcceptAnything           ANY RD RA RB    => LDHI RD 12
-                                                    ADHI RD 212
+        AcceptAnything           ANY RD RA RB    => LDHH RD 12
+                                                    ADDH RD 212
                                                     DYAD RD RA RB
 
-        KleeneImplication        KIMP RD RA RB   => LDHI RD 8
-                                                    ADHI RD 256
+        KleeneImplication        KIMP RD RA RB   => LDHH RD 8
+                                                    ADDH RD 256
                                                     DYAD RD RA RB
 
-        LukasiewiczImplication   LIMP RD RA RB   => LDHI RD 8
-                                                    ADHI RD 337
+        LukasiewiczImplication   LIMP RD RA RB   => LDHH RD 8
+                                                    ADDH RD 337
                                                     DYAD RD RA RB
 
       One-operand functions using DYAD - - - - - - - - - - - - - - - - - - - - -
 
-        NegativeThresholdInvert  NTI RD RA       => LDHI RD -3
-                                                    ADHI RD -78
-                                                    DYAD RD RA R0
+        NegativeThresholdInvert  NTI RD RA       => LDHH RD -3
+                                                    ADDH RD -78
+                                                    DYAD RD RA S0
 
-        PositiveThresholdInvert  PTI RD RA       => LDHI RD -3
-                                                    ADHI RD 84
-                                                    DYAD RD RA R0
+        PositiveThresholdInvert  PTI RD RA       => LDHH RD -3
+                                                    ADDH RD 84
+                                                    DYAD RD RA S0
 
-        TritwiseIncrement        TWI RD RA       => LDHI RD -3
-                                                    ADHI RD 81
-                                                    DYAD RD RA R0
+        TritwiseIncrement        TWI RD RA       => LDHH RD -3
+                                                    ADDH RD 81
+                                                    DYAD RD RA S0
 
-        TritwiseDecrement        TWD RD RA       => LDHI RD 0
-                                                    ADHI RD -78
-                                                    DYAD RD RA R0
+        TritwiseDecrement        TWD RD RA       => LDHH RD 0
+                                                    ADDH RD -78
+                                                    DYAD RD RA S0
 
-        TritwiseIsFalse          TIF RD RA       => LDHI RD -3
-                                                    ADHI RD -78
-                                                    DYAD RD RA R0
+        TritwiseIsFalse          TIF RD RA       => LDHH RD -3
+                                                    ADDH RD -78
+                                                    DYAD RD RA S0
 
-        TritwiseIsUnknown        TIU RD RA       => LDHI RD -3
-                                                    ADHI RD 78
-                                                    DYAD RD RA R0
+        TritwiseIsUnknown        TIU RD RA       => LDHH RD -3
+                                                    ADDH RD 78
+                                                    DYAD RD RA S0
 
-        TritwiseIsTrue           TIT RD RA       => LDHI RD 3
-                                                    ADHI RD -84
-                                                    DYAD RD RA R0
+        TritwiseIsTrue           TIT RD RA       => LDHH RD 3
+                                                    ADDH RD -84
+                                                    DYAD RD RA S0
 
-        TritwiseClampDown        TCD RD RA       => LDHI RD 0
-                                                    ADHI RD -3
-                                                    DYAD RD RA R0
+        TritwiseClampDown        TCD RD RA       => LDHH RD 0
+                                                    ADDH RD -3
+                                                    DYAD RD RA S0
 
-        TritwiseClampUp          TCU RD RA       => LDHI RD 3
-                                                    ADHI RD 0
-                                                    DYAD RD RA R0
+        TritwiseClampUp          TCU RD RA       => LDHH RD 3
+                                                    ADDH RD 0
+                                                    DYAD RD RA S0
     }
   );
 
@@ -301,7 +301,7 @@ begin
       ocLoadHighImmediate:
         if LRegD <> regZero then
           Registers[LRegD] := LImmediateHalf * 729; { << 6 }
-      ocAddImmediateShort:
+      ocAddImmediateQuarter:
         if LRegD <> regZero then
           Registers[LRegD] := Registers[LRegA] + LImmediateQuarter;
       ocAddImmediateHalf:
@@ -332,7 +332,7 @@ begin
         if LRegD <> regZero then
           Registers[LRegD] += 1;
       end;
-      ocPop: begin
+      ocPull: begin
         if LRegD <> regZero then
           Registers[LRegD] -= 1;
         Registers[LRegA] := Memory[Registers[LRegD]];
@@ -399,40 +399,40 @@ begin
   ProgramCounter := 0;
 
   (*
-  OpImm6(ocLoadHighImmediate,    regUser11, 14);                   { LDI U11 10000     } {         }
+  OpImm6(ocLoadHighImmediate,    regUser11, 14);                   { LDW U11 10000     } {         }
   OpImm6(ocAddImmediateHalf,     regUser11, -206);                                       {         }
-  OpImm6(ocLoadLowImmediate,     regUser1, 0);                     { LDLI U1 0         } {         }
-  OpImm6(ocLoadLowImmediate,     regUser2, 0);                     { LDLI U2 0         } {         }
-  OpImm6(ocAddImmediateHalf,     regUser1, 1);                     { ADHI U1 1         } { <<<<<<+ }
-  OpImm3(ocAddImmediateShort,    regUser12, regProgramCounter, 4); { ADSI U12 S1 4     } {       ^ }
+  OpImm6(ocLoadLowImmediate,     regUser1, 0);                     { LDLH U1 0         } {         }
+  OpImm6(ocLoadLowImmediate,     regUser2, 0);                     { LDLH U2 0         } {         }
+  OpImm6(ocAddImmediateHalf,     regUser1, 1);                     { ADDH U1 1         } { <<<<<<+ }
+  OpImm3(ocAddImmediateQuarter,  regUser12, regProgramCounter, 4); { ADDQ U12 S1 4     } {       ^ }
   OpRgtr(ocBranchNotEquals,      regUser12, regUser11, regUser1);  { BRNE U12 U11 U1   } { >>+   ^ }
-  OpImm6(ocLoadHighImmediate,    regUser1, -14);                   { LDI U1 -10000     } {   V   ^ }
+  OpImm6(ocLoadHighImmediate,    regUser1, -14);                   { LDW U1 -10000     } {   V   ^ }
   OpImm6(ocAddImmediateHalf,     regUser1, 206);                                         {   V   ^ }
-  OpImm6(ocAddImmediateHalf,     regUser2, 1);                     { ADHI U2 1         } {   V   ^ }
-  OpImm6(ocAddImmediateHalf,     regProgramCounter, -7);           { ADHI S1 -7        } { <<+ >>+ }
+  OpImm6(ocAddImmediateHalf,     regUser2, 1);                     { ADDH U2 1         } {   V   ^ }
+  OpImm6(ocAddImmediateHalf,     regProgramCounter, -7);           { ADDH S1 -7        } { <<+ >>+ }
   // *)
 
   //(*
-  OpImm6(ocLoadHighImmediate,    regUser1, 13);                    { LDI U1 9464       } {         } { LOAD OPERAND A: 000+++000---             }
+  OpImm6(ocLoadHighImmediate,    regUser1, 13);                    { LDW U1 9464       } {         } { LOAD OPERAND A: 000+++000---             }
   OpImm6(ocAddImmediateHalf,     regUser1, -13);                                         {         }
-  OpImm6(ocLoadHighImmediate,    regUser2, 224);                   { LDI U2 163520     } {         } { LOAD OPERAND B: +0-+0-+0-+0-             }
+  OpImm6(ocLoadHighImmediate,    regUser2, 224);                   { LDW U2 163520     } {         } { LOAD OPERAND B: +0-+0-+0-+0-             }
   OpImm6(ocAddImmediateHalf,     regUser2, 224);                                         {         }
-  OpImm6(ocLoadHighImmediate,    regUser10, 5);                    { LDI U10 3445      } {         } { LOAD TRUTH TABLE FOR TRITWISE EQUALITY   }
+  OpImm6(ocLoadHighImmediate,    regUser10, 5);                    { LDW U10 3445      } {         } { LOAD TRUTH TABLE FOR TRITWISE EQUALITY   }
   OpImm6(ocAddImmediateHalf,     regUser10, -200);                                       {         }
-  OpRgtr(ocAddRegister,          regUser3, regUser10, regZero);    { MOVR U3 U10       } {         } { MOVE TRUTH TABLE TO DESTINATION REGISTER }
+  OpRgtr(ocAddRegister,          regUser3, regUser10, regZero);    { MOVR U3 U10       } {         } { MOVR TRUTH TABLE TO DESTINATION REGISTER }
   OpRgtr(ocDyadicFunction,       regUser3, regUser1, regUser2);    { DYAD U3 U1 U2     } {         } { PERFORM TRITWISE EQUALITY OPERATION      }
   OpRgtr(ocReserved13,           regZero, regZero, regZero);       { HALT              } {         }
   // *)
 
   (*
-  OpImm6(ocLoadHighImmediate,    regUser1, 224);                   { LDI U2 163520     } {         } { LOAD TEST WORD WITH NICE PATTERN         }
+  OpImm6(ocLoadHighImmediate,    regUser1, 224);                   { LDW U2 163520     } {         } { LOAD TEST WORD WITH NICE PATTERN         }
   OpImm6(ocAddImmediateHalf,     regUser1, 224);                                         {         }
-  OpImm6(ocLoadLowImmediate,     regUser2, -12);                   { LDLI U2 -12       } {         } { CURRENT SHIFT AMOUNT                     }
-  OpImm6(ocLoadLowImmediate,     regUser3, 12);                    { LDLI U3 12        } {         } { MAX SHIFT AMOUNT                         }
-  OpImm3(ocAddImmediateShort,    regUser4, regProgramCounter, 0);  { MOVR U4 S1        } {         } { SET BRANCH TARGET TO NEXT INSTRUCTION    }
-  OpRgtr(ocShift,                regUser10, regUser1, regUser2);   { SHFT U10 U1 U2    } { <<+     }
+  OpImm6(ocLoadLowImmediate,     regUser2, -12);                   { LDLH U2 -12       } {         } { CURRENT SHIFT AMOUNT                     }
+  OpImm6(ocLoadLowImmediate,     regUser3, 12);                    { LDLH U3 12        } {         } { MAX SHIFT AMOUNT                         }
+  OpImm3(ocAddImmediateQuarter,  regUser4, regProgramCounter, 0);  { MOVR U4 S1        } {         } { SET BRANCH TARGET TO NEXT INSTRUCTION    }
+  OpRgtr(ocShift,                regUser10, regUser1, regUser2);   { LSHR U10 U1 U2    } { <<+     }
   OpRgtr(ocRotate,               regUser11, regUser1, regUser2);   { ROTR U11 U1 U2    } {   ^     }
-  OpImm3(ocAddImmediateShort,    regUser2, regUser2, 1);           { ADSI U2 U2 1      } {   ^     }
+  OpImm3(ocAddImmediateQuarter,  regUser2, regUser2, 1);           { ADDQ U2 U2 1      } {   ^     }
   OpRgtr(ocBranchLessEqualsThan, regUser4, regUser2, regUser3);    { BRNE U4 U2 U3     } { >>+     }
   OpRgtr(ocReserved13,           regZero, regZero, regZero);       { HALT              } {         }
   // *)
