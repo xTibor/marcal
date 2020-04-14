fc7963c14e33addadc497c9c16067ddd15d9378e
Tibor Nagy <xnagytibor@gmail.com>
2020-04-14 22:11:56 +0200

    Remove unnecessary dyadic function constants

--- src/Trit.pas
+++ src/Trit.pas
@@ -10,42 +10,7 @@ const
 
 type
   TTritArray = array of TTrit;
-
-{ Dyadic functions }
-
-type
   TTritDyadicFunction = array[TTrit, TTrit] of TTrit;
-
-const
-  CTritFunctionAnd: TTritDyadicFunction = (
-    (-1, -1, -1),
-    (-1,  0,  0),
-    (-1,  0,  1)
-  );
-
-  CTritFunctionOr: TTritDyadicFunction = (
-    (-1,  0,  1),
-    ( 0,  0,  1),
-    ( 1,  1,  1)
-  );
-
-  CTritFunctionAddSum: TTritDyadicFunction = (
-    ( 1, -1,  0),
-    (-1,  0,  1),
-    ( 0,  1, -1)
-  );
-
-  CTritFunctionAddCarry: TTritDyadicFunction = (
-    (-1,  0,  0),
-    ( 0,  0,  0),
-    ( 0,  0,  1)
-  );
-
-  CTritFunctionEquality: TTritDyadicFunction = (
-    ( 1, -1, -1),
-    (-1,  1, -1),
-    (-1, -1,  1)
-  );
 
 implementation
 
