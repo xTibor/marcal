38c1d645332e80b8c550227e8135c463bd025b74
Tibor Nagy <xnagytibor@gmail.com>
2020-04-21 17:58:09 +0200

    Reorder branches to do less work for zero trits

--- asm/Multiply.s
+++ asm/Multiply.s
@@ -11,7 +11,7 @@ INITMULT   LDLH U4 11                         COUNTER
            LDLH U7 0                          TEMPORARY 2
            LDHH U8 364                        PATTERN WITH EVERY TRIT SET
            ADDH U8 364
-           ADDQ U9 PC 9                       BRANCH TARGET 1
+           ADDQ U9 PC 10                      BRANCH TARGET 1
            ADDQ U10 PC 1                      BRANCH TARGET 2
            ADDQ U11 PC 13                     BRANCH TARGET 3
 
@@ -20,12 +20,11 @@ LOOPSTART  NEGR U6 U4 ZERO                    GET THE NTH BIT OF OPERAND B
            LSHI U6 U6 11
            LSHI U6 U6 -11
 
-EXPPLUS    BRLE U9 U6 ZERO
-           ADDR U6 U8 ZERO                    EXPAND THE RIGHTMOST TRIT AS PLUS
-           ADDH PC 2
-
-EXPMINUS   BRLE U11 ZERO U6
-           NEGR U6 U8 ZERO                    EXPAND THE RIGHTMOST TRIT AS MINUS
+           BREQ U11 U6 ZERO
+           BRLT U9 U6 ZERO
+EXPPLUS    ADDR U6 U8 ZERO                    EXPAND THE RIGHTMOST TRIT AS PLUS
+           ADDH PC 1
+EXPMINUS   NEGR U6 U8 ZERO                    EXPAND THE RIGHTMOST TRIT AS MINUS
 
 SHIFTADD   ADDR U7 U5 ZERO                    LOAD TRUTH TABLE
            DYAD U7 U1 U6                      MULTIPLY OPERAND A WITH EXPANDED
